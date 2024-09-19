43f8d993e67b5cceb7729cc7b48ef50563668e8e
seasonllybraind <80056392+seasonllybraind@users.noreply.github.com>
2024-09-19 09:46:47 -0400

    added some templating rendering so I can have the text live on my website

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,1 +1,21 @@
-<h1><?= $page->title() ?></h1>
+<html dir="ltr" lang="en-US">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+
+  <title>I am Jonathan Stephens and I design organizations, teams, and products.</title>
+
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+</head>
+
+<body>
+
+<main>
+  <h1><?= $page->title() ?></h1>
+  <?= $page->text()->kirbytext() ?>
+</main>
+
+<footer>
+</footer>
+
+</html>

0b773f0ab7e8151ac6980d772bad536286a88be4
seasonllybraind <80056392+seasonllybraind@users.noreply.github.com>
2024-09-27 09:45:12 -0400

    starting the codex design system reference pages, first focusing on colors...long way to go but  needed to play with the color generator to get it to work properly and test out the colors themselves

---
+++ site/templates/codex.php
@@ -0,0 +1,114 @@
+<?php snippet('header') ?>
+  <section class="wrapper">
+    <?php snippet('components/breadcrumb') ?>
+
+    <div class="splash">
+      <?= $page->text()->kirbytext() ?>
+
+      <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+        <rect width="160" height="160" fill="var(--palette-cyan-50)" />
+      </svg>
+      <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+        <rect width="160" height="160" fill="var(--palette-magenta-50)" />
+      </svg>
+
+      <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+        <rect width="160" height="160" fill="var(--palette-yellow-50)" />
+      </svg>
+      <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+        <rect width="160" height="160" fill="var(--palette-green-50)" />
+      </svg>
+
+      <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+        <rect width="160" height="160" fill="var(--palette-purple-50)" />
+      </svg>
+
+      <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+        <rect width="160" height="160" fill="var(--palette-orange-50)" />
+      </svg>
+
+ <hr />
+
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklch shorter hue, var(--palette-cyan-50), var(--palette-green-50))" />
+ </svg>
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklch shorter hue, var(--palette-yellow-50), var(--palette-green-50))" />
+ </svg>
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklch shorter hue, var(--palette-magenta-50), var(--palette-green-50))" />
+ </svg>
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklch shorter hue, var(--palette-green-50), var(--palette-green-50))" />
+ </svg>
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklch shorter hue, var(--palette-purple-50), var(--palette-green-50))" />
+ </svg>
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklch shorter hue, var(--palette-orange-50), var(--palette-green-50))" />
+ </svg>
+ <br/>
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklab, var(--palette-cyan-50), var(--palette-green-50))" />
+ </svg>
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklab, var(--palette-yellow-50), var(--palette-green-50))" />
+ </svg>
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklab, var(--palette-magenta-50), var(--palette-green-50))" />
+ </svg>
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklab, var(--palette-green-50), var(--palette-green-50))" />
+ </svg>
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklab, var(--palette-purple-50), var(--palette-green-50))" />
+ </svg>
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklab, var(--palette-orange-50), var(--palette-green-50))" />
+ </svg>
+
+ <hr/>
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklch shorter hue, var(--palette-cyan-50), var(--palette-purple-50))" />
+ </svg>
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklch shorter hue, var(--palette-yellow-50), var(--palette-purple-50))" />
+ </svg>
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklch shorter hue, var(--palette-magenta-50), var(--palette-purple-50))" />
+ </svg>
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklch shorter hue, var(--palette-green-50), var(--palette-purple-50))" />
+ </svg>
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklch shorter hue, var(--palette-purple-50), var(--palette-purple-50))" />
+ </svg>
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklch shorter hue, var(--palette-orange-50), var(--palette-purple-50))" />
+ </svg>
+ <hr/>
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklch shorter hue, var(--palette-cyan-50), var(--palette-orange-50))" />
+ </svg>
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklch shorter hue, var(--palette-yellow-50), var(--palette-orange-50))" />
+ </svg>
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklch shorter hue, var(--palette-magenta-50), var(--palette-orange-50))" />
+ </svg>
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklch shorter hue, var(--palette-green-50), var(--palette-orange-50))" />
+ </svg>
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklch shorter hue, var(--palette-purple-50), var(--palette-orange-50))" />
+ </svg>
+ <svg width="160" height="160" xmlns="http://www.w3.org/2000/svg">
+   <rect width="160" height="160" fill="color-mix(in oklch shorter hue, var(--palette-orange-50), var(--palette-orange-50))" />
+ </svg>
+ <hr/>
+
+
+
+    </div>
+  </section>
+<?php snippet('footer') ?>

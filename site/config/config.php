a86411699d1094e603f9d15b75244d64e967edee
Jonathan Stephens <jonathan@poetandscribe.com>
2024-11-18 11:42:39 -0500

    trying to add my plugins...but they break at the moment. still keeping the turning on there. also indieconnector's been added but havne't figured out how to use it et

--- site/config/config.php
+++ site/config/config.php
@@ -1,7 +1,22 @@
 <?php
   return [
     'panel' =>[
-      'debug' => false
+      'debug' => false,
+      'mauricerenck.indieConnector.secret' => 'my-secret',
+    ],
+
+    'jonathanstephens.template-handler' => [
+        'cache' => true,
+        'defaultTemplate' => 'default'
+    ],
+    'jonathanstephens.timekeeper' => [
+        'useClientTime' => true,
+        'allowManualOverride' => true,
+        'cookieDuration' => 60 * 60 * 24
     ]
   ];
+
+  return [
+];
+
 ?>

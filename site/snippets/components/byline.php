dd147fdc7d9ac90aa8138ef8a9fc71b740cd72e5
seasonllybraind <80056392+seasonllybraind@users.noreply.github.com>
2024-11-22 10:38:40 -0500

    Updated byline for better html...shouldn't be an h2...and byline is assumed here.

--- site/snippets/components/byline.php
+++ site/snippets/components/byline.php
@@ -1,5 +1,4 @@
-<h2 class="byline">
+<p class="byline">
   <span class="author">Jonathan Stephens</span>
   <time class="dt-published" datetime="<?= $page->metadata()->date()->toDate('F j Y') ?> <?= $page->metadata()->time()->toDate('H:i') ?>" itemprop="dateCreated pubdate datePublished"><?= $page->metadata()->date()->toDate('M j Y') ?></time>
-  <?= $page->metadata()->location() ?>
-</h2>
+</p>

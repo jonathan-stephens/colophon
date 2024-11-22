1bb3c15fcbed7a8470e90157d970f88b7fd8bdb9
seasonllybraind <80056392+seasonllybraind@users.noreply.github.com>
2024-11-22 10:39:29 -0500

    Wrapped the article listing box in an anchor to have the full block linkable rather than just the text.

--- site/templates/journal-container.php
+++ site/templates/journal-container.php
@@ -8,13 +8,13 @@
 <?php foreach($page->children()->listed()->flip() as $article): ?>
 
   <article class="cluster">
-    <box-l>
-      <a href="<?= $article->url() ?>">
-        <h2><?= $article->hed()->html() ?></h2>
-        <p class="subtitle"><?= $article->dek()->html() ?></p>
-        <time datetime="<?= $article->metadata()->date()->toDate('F j Y') ?> <?= $article->metadata()->time()->toDate('H:i') ?>"><?= $article->metadata()->date()->toDate('M j Y') ?></time>
-      </a>
-    </box-l>
+    <a href="<?= $article->url() ?>">
+      <box-l>
+          <h2><?= $article->hed()->html() ?></h2>
+          <p class="subtitle"><?= $article->dek()->html() ?></p>
+          <time datetime="<?= $article->metadata()->date()->toDate('F j Y') ?> <?= $article->metadata()->time()->toDate('H:i') ?>"><?= $article->metadata()->date()->toDate('M j Y') ?></time>
+      </box-l>
+    </a>
   </article>
 
   <?php endforeach ?>

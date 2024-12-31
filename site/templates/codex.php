0457d35fdaae61308103371c3d7b77028a4e39ec
Jonathan Stephens <jonathan@poetandscribe.com>
2024-12-31 11:51:46 +0100

    Updated styleguide to help make some decisions and see all the things

--- site/templates/codex.php
+++ site/templates/codex.php
@@ -120,6 +120,89 @@
   <div style="border-width:var(--border-ultra)"></div>
 </div>
 
+<div class="typographic-styles">
+  <div class="root-variables">
+    <div class="text-weights">
+      <p class="weight-text" style="font-weight:var(--weight-text)">
+        This is my sample sentence, in a text weight.
+      </p>
+      <p class="weight-medium" style="font-weight:var(--weight-medium)">
+        This is my sample sentence, in a medium weight.
+      </p>
+      <p class="weight-semibold" style="font-weight:var(--weight-semibold)">
+        This is my sample sentence, in a semibold weight.
+      </p>
+      <p class="weight-bold" style="font-weight:var(--weight-bold)">
+        This is my sample sentence, in a bold weight.
+      </p>
+    </div>
+    <div class="text-headings">
+      <p class="hed-primary" style="font-size:var(--hed-primary)">
+        Hed: Primary
+      </p>
+      <p class="hed-primary" style="font-size:var(--hed-secondary)">
+        Hed: Secondary
+      </p>
+      <p class="hed-primary" style="font-size:var(--hed-tertiary)">
+        Hed: Tertiary
+      </p>
+      <p class="hed-primary" style="font-size:var(--hed-quaternary)">
+        Hed: Quaternary
+      </p>
+      <p class="hed-primary" style="font-size:var(--hed-quinary)">
+        Hed: Quinary
+      </p>
+    </div>
+    <div class="text-copy"</div>
+      <p class="prose" style="font-size:var(--prose)">
+        This is a prose-sized paragraph, regular sorta thing. Ned to copy and paste  a real paragrah here
+      </p>
+      <p class="prose" style="font-size:var(--prose)">
+        This is a prose-sized paragraph, regular sorta thing. Ned to copy and paste  a real paragrah here
+      </p>
+      <blockquote>
+        This is a blockquote
+      </blockquote>
+      <p class="prose" style="font-size:var(--prose)">
+        This is a prose-sized paragraph, regular sorta thing. Ned to copy and paste  a real paragrah here
+      </p>
+
+      <p class="small" style="font-size:var(--small)">
+        This is a small-sized paragraph, regular sorta thing. Ned to copy and paste  a real paragrah here
+      </p>
+      <p class="caption" style="font-size:var(--caption)">
+        This is a caption-sized paragraph, regular sorta thing. Ned to copy and paste  a real paragrah here
+      </p>
+    </div>
+    <div class="text-combos flow">
+      <div>
+        <h1><strong>p+h1: </strong>Article > Primary Header</h1>
+        <p><strong>h1+p: </strong>Article > Primary Header > First Paragraph</p>
+      </div>
+
+      <div>
+        <h2><strong>p+h2: </strong>Article > Secondary Header</h2>
+        <p><strong>h2+p: </strong>Article > Secondary Header > First Paragraph</p>
+      </div>
+      <div>
+        <h3><strong>p+h3: </strong>Article > Tertiary Header</h3>
+        <p><strong>h3+p: </strong>Article > Tertiary Header > First Paragraph</p>
+      </div>
+      <div>
+        <h4><strong>p+h4: </strong>Article > Quaternary Header</4>
+        <p><strong>h4+p: </strong>Article > Quaternary Header > First Paragraph</p>
+      </div>
+      <div>
+        <h5><strong>p+h5: </strong>Article > Quinary Header</h5>
+        <p><strong>h5+p: </strong>Article > Quinary Header > First Paragraph</p>
+      </div>
+    </div>
+    <div class="figures">
+      
+    </div>
+
+  </div>
+</div>
     </div>
   </section>
 <?php snippet('footer') ?>

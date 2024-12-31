d3866264fe2c76b74e72f6d04448d32b1176e27c
Jonathan Stephens <jonathan@poetandscribe.com>
2024-12-31 11:51:33 +0100

    Working on the journal post page and layout

--- site/templates/journal.php
+++ site/templates/journal.php
@@ -1,24 +1,19 @@
 <?php snippet('header') ?>
 
-  <article class="article wrapper">
-      <h1 class="p-name" itemprop="headline"><?= $page->title()->html() ?></h1>
-      <?php snippet('/components/byline') ?>
+  <article class="article post wrapper" itemscope itemtype="http://schema.org/Article">
+    <header>
+      <h1 class="p-name" itemprop="name headline"><?= $page->hed()->html() ?></h1>
+      <p><?= $page->dek()->html() ?><p>
+      <?php snippet('/components/on-at-in') ?>
+    </header>
+
 
     <div class="e-content prose" itemprop="articleBody">
       <?= $page->text()->kirbytext() ?>
     </div>
 
-    <footer>
-      <p class="meta">
-        <span class="p-location h-geo">
-          <data class="p-latitude" value=""></data>
-          <data class="p-longitude" value=""></data>
-        </span>
-        <time datetime="<?= $page->metadata()->date()->toDate('F j Y') ?> <?= $page->metadata()->time()->toDate('H:i') ?>" itemprop="dateCreated pubdate datePublished"><?= $page->metadata()->time()->toDate('H:i') ?></time>
-      </p>
-      <p class="meta">
-        Tags
-      </p>
+    <footer class="meta wrapper">
+      <?php snippet('/components/tags') ?>
     </footer>
 
   </article>

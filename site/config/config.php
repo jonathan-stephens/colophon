d3fd0f9e0391ed00d20b33191bb23b8974fcaa78
Jonathan Stephens <jonathan@poetandscribe.com>
2025-01-03 15:42:14 +0100

    Adding Content to Journal from Substack/500 Characters

--- site/config/config.php
+++ site/config/config.php
@@ -12,18 +12,5 @@ return [
         'useClientTime' => true,
         'allowManualOverride' => true,
         'cookieDuration' => 60 * 60 * 24
-    ],
-    'routes' => [
-        [
-            'pattern' => 'tag/(:any)',
-            'action'  => function($tag) {
-                return page('default')->render([
-                    'template' => 'tag',
-                    'data' => [
-                        'tag' => urldecode($tag)
-                    ]
-                ]);
-            }
-        ]
     ]
 ];
---
+++ site/templates/colors.php
@@ -0,0 +1,26 @@
+<?php snippet('head') ?>
+
+<body>
+  <?php if($page->isHomePage()): ?>
+    <section class="splash wrapper">
+      <?= $page->text()->kirbytext() ?>
+
+      <div class="cluster">
+        <a class="button" href="https://linkedin.com/in/elnatnal">
+          Connect on LinkedIn
+        </a>
+        <a class="button" href="https://jonathanstephens.substack.com/">
+          Subscribe on Substack
+        </a>
+        <a class="button"  href="https://cal.com/jonathanstephens/book">
+          Book time on my calendar
+        </a>
+      <div>
+    </section>
+  <?php else: ?>
+    <?= $page->text()->kirbytext() ?>
+  <?php endif ?>
+</body>
+<footer>
+  <?php snippet('copyright') ?>
+</footer>

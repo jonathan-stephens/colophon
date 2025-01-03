0808259bca9eb8434bd943b37c7b6e6e9191d3bf
Jonathan Stephens <jonathan@poetandscribe.com>
2025-01-03 13:48:07 +0100

    Split out parts of the head and footer so I can create one off pages but keep the metadata and copyright information

---
+++ site/snippets/head.php
@@ -0,0 +1,39 @@
+<html dir="ltr" lang="en-US">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+
+  <title>
+    <?= $site->title()->esc() ?> | <?php if($page->isHomePage()): ?>Home<?php else: ?><?= $page->title()->esc() ?><?php endif ?>
+
+  </title>
+
+  <!-- webmention -->
+  <link rel="pingback" href="https://webmention.io/jonathanstephens.us/xmlrpc" />
+  <link rel="webmention" href="https://webmention.io/jonathanstephens.us/webmention" />
+  <link rel="micropub" href="https://jonathanstephens.us/micropub">
+  <link rel="authorization_endpoint" href="https://indieauth.com/auth">
+  <link href="https://github.com/jonathan-stephens" rel="me">
+
+
+  <?php snippet('webmention-endpoint'); ?>
+
+
+  <?= css([
+    'assets/css/main.css',
+  ]) ?>
+
+  <noscript>
+    <style>
+      #theme-picker {
+        display: none;
+      }
+    </style>
+  </noscript>
+  <?php
+    $period = TimeKeeper::getCurrentPeriod();
+    $season = TimeKeeper::getCurrentSeason();
+    ?>
+</head>

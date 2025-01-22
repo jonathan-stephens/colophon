86b7e2f715c5adb659012458fcb1a199d67a2e55
Jonathan Stephens <jonathan@poetandscribe.com>
2025-01-22 09:46:02 -0500

    Added time-to-read and moved some things around slightly for that.
    right now, everything's. in. /journal. I want to add the time to read to journal entries and essay entries.

---
+++ site/config/methods.php
@@ -0,0 +1,3 @@
+<?php
+
+$field->words(): int
--- site/templates/journal-container.php
+++ site/templates/journal-container.php
@@ -10,7 +10,34 @@
   <article class="h-entry">
     <a href="<?= $article->url() ?>">
       <box-l class="e-content">
-          <time class="dt-published" datetime="<?= $article->metadata()->date()->toDate('F j Y') ?> <?= $article->metadata()->time()->toDate('H:i') ?>"><?= $article->metadata()->date()->toDate('j M Y') ?></time>
+          <div class="meta">
+            <time class="dt-published" datetime="<?= $article->metadata()->date()->toDate('F j Y') ?> <?= $article->metadata()->time()->toDate('H:i') ?>"><?= $article->metadata()->date()->toDate('j M Y') ?></time>
+            <p class="text-stats">
+              <?php
+                $wordCount = $article->text()->words();
+                $minSpeed = 167; // words per minute
+                $maxSpeed = 285; // words per minute
+
+                $minSeconds = ceil($wordCount / ($minSpeed / 60));
+                $maxSeconds = ceil($wordCount / ($maxSpeed / 60));
+
+                if ($minSeconds < 60) {
+                  if ($minSeconds === $maxSeconds) {
+                    echo $minSeconds . ' sec read';
+                  } else {
+                    echo $maxSeconds . '&thinsp;–&thinsp;' . $minSeconds . '  sec read';
+                  }
+                } else {
+                  $minMinutes = ceil($minSeconds / 60);
+                  $maxMinutes = ceil($maxSeconds / 60);
+                  if ($minMinutes === $maxMinutes) {
+                    echo $minMinutes . ' min read';
+                  } else {
+                    echo $maxMinutes . '&thinsp;–&thinsp;' . $minMinutes . ' min read';
+                  }
+                }
+              ?>
+          </div>
           <h2 class="p-name hed"><?= $article->hed()->html() ?></h2>
           <p class="dek"><?= $article->dek()->html() ?></p>
       </box-l>

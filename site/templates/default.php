750deb3994a27a372d04390287c3412fbf93aafd
seasonllybraind <80056392+seasonllybraind@users.noreply.github.com>
2025-03-03 16:38:50 -0500

    Removing all older files that I had overcomplicated making pages and such. Simplifying for now.

--- site/templates/default.php
+++ site/templates/default.php
@@ -19,12 +19,18 @@
   <?php else: ?>
     <article class="default wrapper">
       <header>
-        <h1 class="p-name" itemprop="name headline"><?= $page->title() ?></h1>
+        <h1 class="p-name" itemprop="name headline"><?= $page->hed()->html() ?></h1>
+        <p><?= $page->dek()->html() ?><p>
+        <?php snippet('/components/on-at-in') ?>
       </header>
 
       <div class="e-content prose" itemprop="articleBody">
         <?= $page->text()->footnotes() ?>
       </div>
+
+      <footer class="meta">
+        <?php snippet('/components/tags', ['reference' => $page]) ?>
+      </footer>
     </article>
   <?php endif ?>
 <?php snippet('footer') ?>

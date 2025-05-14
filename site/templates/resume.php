822808753f2c8b72e2a321d363fff4d5cfa811ea
seasonllybraind <80056392+seasonllybraind@users.noreply.github.com>
2025-05-14 10:21:28 -0400

    Created a /resume page to start marking it up in microformats.
    After marking up and all, I'll work on making it templatable for others in Kirby to have a microformat resume creator.

---
+++ site/templates/resume.php
@@ -0,0 +1,133 @@
+<?php snippet('header') ?>
+
+  <article class="article h-resume post wrapper" itemscope itemtype="http://schema.org/Article">
+    <header>
+      <h1 class="p-name" itemprop="name headline"><?= $page->hed()->html() ?></h1>
+      <p><?= $page->dek()->html() ?><p>
+      <?php snippet('/components/on-at-in') ?>
+    </header>
+
+    <aside class="contact p-contact h-card">
+      <span class="p-fn p-name" hidden>Jonathan Stephens</span>
+      <span class="p-nickname" hidden>Jonathan</span>
+      <span class="p-title" hidden>Product Development, Digital Designer, & Tech Leader with 18+ years experience</span>
+      <h3>Contact me</h3>
+      <ul>
+        <li title="email"><a href="mailto:hello@jonathanstephens.us" class="u-email">hello@jonathanstephens.us</a></li>
+        <li title="mobile"><a class="p-tel" href="tel:+19842187034">+19842187034</a></li>
+        <li title="website"><a href="https://jonathanstephens.us" class="u-url website" rel="me self external">jonathanstephens.us</a></li>
+        <li title="github"><a href="https://github.com/jonathan-stephens" class="u-url github" rel="me self external">jonathan-stephens</a></li>
+        <li title="linkedin"><a href="https://www.linkedin.com/in/elnatnal" class="u-url linkedin" rel="me self external">elnatnal</a></li>
+        <li title="address">
+          <address class="p-adr h-adr">
+            <span class="p-locality">Raleigh</span>,
+            <span class="p-region">North Carolina</span>,
+            <span class="p-country-name">United States</span>
+          </address>
+        </li>
+      </ul>
+    </aside>
+
+    Jonathan Stephens
+    Director of Product Design & Development | Head of Digital Products & Experience
+    United States (Open to Relocation) | +1 (984) 218–7034 | jonathanmstephens@gmail.com |
+    Website:  https://jonathanstephens.us | ADP List: https://adplist.org/mentors/jonathan-stephens |
+    https://jonathanstephens.blog  | Linkedin: https://www.linkedin.com/in/elnatnal/
+
+    <p class="summary">
+      Experienced Product & UX Leader with 17+ years of expertise in product strategy, design systems, and digital transformation across B2C and B2B platforms. Adept at leading cross-functional teams of designers, engineers, and product managers to drive scalable, user-centered solutions that enhance business growth and customer experience. Proven track record in data-driven decision-making, A/B testing, and accessibility compliance (WCAG, Section 508) to optimize engagement and usability. Strong background in change management, organizational scaling, and Agile leadership, ensuring seamless collaboration across startups, enterprises, and government projects. Passionate about inclusive design, creativity, and building high-performance teams to create impactful, future-ready products.
+    </p>
+
+    SKILLS / Areas of Expertise: Product Strategy & Roadmap Development - UX/UI Leadership & Human-Centered Design - Design Systems & Scalable Digital Solutions - Agile & Cross-Functional Team Leadership - Data-driven decision-making making & A/B Testing - Stakeholder Management & Executive Communication - Business Growth & Digital Transformation - Accessibility & Inclusive Design (WCAG, Section 508 Compliance) - Change Management & Organizational Scaling - People Development & High-Performance Team Building
+    LANGUAGES: English: Native | Spanish: Conversational | Dutch: Conversational.
+    PROFESSIONAL EXPERIENCE
+    INDEPENDENT CONSULTANT, PRODUCT DESIGN | Strategi Consulting | Remote
+    OCTOBER 2024 – PRESENT
+    Leading UX/UI strategy and end-to-end product design for a 0 → 1 strategic web application for the U.S. Space Force, enhancing operational decision-making through data-driven solutions.
+    Conducting user research, stakeholder interviews, and competitive analysis to define product requirements and optimize user experience.
+    Developing wireframes, prototypes, and a scalable design system, ensuring accessibility (WCAG, Section 508) and alignment with government security standards.
+    Collaborating with engineers, data scientists, and government stakeholders to integrate usability, technical feasibility, and compliance into the product roadmap.
+    Leading stakeholder workshops to streamline decision-making, define MVP priorities, and ensure efficient resource allocation for scalable product growth.
+    FOUNDER | Poet & Scribe | Raleigh, NC
+    FEBRUARY 2022 – PRESENT
+    Poet & Scribe is an independently owned, multidisciplinary design consultancy specializing in UX/UI design, product development, accessibility, and digital transformation. Through strategic design and innovation, it helps startups and enterprises create scalable, user-centered solutions.
+    INDEPENDENT CONSULTANT, PRODUCT DESIGN | Leantime.io | Remote
+    JULY 2024 – AUGUST 2024
+    Designed and implemented comprehensive design system foundations, improving UI consistency, accessibility, and user experience across the platform.
+    Optimized onboarding flows and user interactions, enhancing usability and engagement for new and existing users.
+    Provided strategic design consulting, advising on long-term product strategy, visual identity, and UX improvements to align with business goals.
+    Collaborated with cross-functional teams, including developers and product managers, to ensure seamless integration of design principles.
+    Led usability reviews and iterative testing, refining user workflows and ensuring a cohesive, intuitive product experience.
+    INDEPENDENT CONSULTANT, BUSINESS DESIGN | Moon Audio | On-site
+    MARCH 2023 – AUGUST 2023
+    Modernized design, collaboration, and content operations, fostering a data-informed culture to drive business growth and efficiency.
+    Led company-wide co-creation sessions, establishing content production processes that enhanced marketing funnel efficiency and initial KPI development.
+    Developed and successfully pitched hiring strategies and interview processes to improve storefront conversion rates, ensuring data-driven decision-making in recruitment.
+    BOOKING.COM  - JANUARY 2013 – DECEMBER 2021 | Amsterdam
+    DIRECTOR OF PRODUCT DESIGN & ENGINEERING | FEBRUARY 2016 - DECEMBER 2021
+    Led global-scale UX and product design for B2C and B2B platforms, managing 30 to 100+ designers, developers, researchers, and engineers teams while driving content, maps, search, and localization strategy across 220+ countries and 40+ languages.
+    Innovated the development of Partner Platform Design Systems, improving UI consistency, accessibility (WCAG compliance), and iteration speed while optimizing A/B testing and data-driven decision-making to enhance user engagement and conversion rates.
+    Navigated large-scale organizational transitions, leadership changes, and crisis management during COVID-19, ensuring team stability, business continuity, and product innovation.
+    Organized and facilitated department-wide conferences (300–500 attendees), fostering knowledge-sharing and cross-functional collaboration across engineering, UX, and product teams.
+    Won two consecutive Playmaker & Inclusionist Awards (Top 35 globally) for championing diversity, inclusion, and belonging initiatives, including creating an internal D&I portal.
+    DESIGN & ENGINEERING MANAGER | 2014 – 2016
+    Led multiple Agile, cross-functional product teams, driving UX, front-end development, and product strategy for customer-facing platforms during hyper-growth.
+    Developed and implemented team vision, talent development programs, and performance KPIs, fostering a high-performing and collaborative design culture.
+    Evolved design system through iterative experimentation, componentization, and process optimization, enhancing product quality, usability, and efficiency across platforms.
+    SENIOR PRODUCT DESIGNER | 2015 – 2016
+    Led UX and product design for key customer-facing products, including checkout, booking process, and post-booking experience, improving conversion and user satisfaction.
+    Managed cross-functional Agile teams of designers, developers, and copywriters, streamlining design workflows and enhancing collaboration across disciplines.
+    Designed and implemented data-driven A/B tests, optimizing user flows and refining interfaces based on behavioral insights and real-time analytics.
+    UX DESIGNER | 2013 –2015
+    Designed end-to-end user experiences for web and mobile, leveraging data-driven design, A/B testing, and user research to optimize engagement and conversion.
+    Developed wireframes, prototypes, and responsive UI components while collaborating with developers, product managers, and copywriters to align business and user needs.
+    Led usability testing and iterative design processes, refining interfaces based on real-time insights to enhance accessibility and cross-platform consistency.
+    EARLY CAREER (CONDENSED)
+    FREELANCER DESIGNER | 2008–2016
+    Provided end-to-end UX/UI design, branding, and digital strategy for startups, agencies, and government projects, delivering responsive web and mobile solutions while collaborating with developers and business stakeholders.
+    VARIOUS DESIGN & UX ROLES (AGENCIES, STARTUPS, GOVERNMENT) | 2013–2015
+    Designed and developed UX/UI solutions for diverse industries, including tech startups, agencies, and government organizations, focusing on responsive design, accessibility, and user research to enhance digital experiences.
+    EDUCATION
+    Bachelor of Graphic Design, Cum Laude – North Carolina State University (2007 – 2011)
+    TRAININGS & COURSES
+    1,000+ Mentorship Minutes – ADPList (2024)
+    Diversity & Inclusion Leadership – Tech Conferences (2018 – 2019)
+    Professional Practice & UX Leadership – NCSU (2022)
+    SPEAKING ENGAGEMENTS & WORKSHOPS
+    “No-code, low-code, gen-ai; oh my!” - Skills Development Group (2024)
+    “Preparing for Professional Practice” - N.C. State University (2022)
+    “Inclusion is hard; it’s the only way forward” - Tech Conference (2019)
+    “Identifying Talent & People Development” - Senior Leadership Training (2018)
+    “Diversity is easy; inclusion is hard” - Tech Conference (2018)
+    “How does the Tech Department work?” - Booking.com Annual Meeting (2017)
+    “10 Principles of Design at Booking.com” - Tech Conference (2015)
+    “Design for Front End” - Onboarding Training & Workshop (2015)
+    “git bisect for Designers” - Community Workshop (2014)
+    “Storyboarding in UX Design” - Community Workshop (2014)
+    “Data-Driven Design”- N.C. State University (2014)
+    “Typography is to the Internet as Butter is to Southern Cooking” - Tech Conference (2013) & Raleigh WordCamp (2012)
+    AWARDS AND RECOGNITIONS
+    Playmaker & Inclusionist Awards (Top 35 Global) | Booking.com (2019, 2020)
+    Adobe Design Achievement Awards – Mobile Design Semi-Finalist (2010)
+    Tech Hackathon Internal Branding Competition Winner (2018)
+    VOLUNTEERING & MENTORSHIP
+    Design Community Mentor | ADPList (2022 – Present)
+    Local Community Volunteer | Raleigh-Durham Startup Week (2024)
+    Works Council VP | Booking.com (2019 – 2021)
+
+
+
+
+
+
+
+
+
+
+    <footer class="meta">
+      <?php snippet('/components/tags', ['reference' => $page]) ?>
+    </footer>
+
+  </article>
+
+<?php snippet('footer') ?>

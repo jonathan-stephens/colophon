bdeb128ecbef5b5ef532dcf119da83b63c63cde1
seasonllybraind <80056392+seasonllybraind@users.noreply.github.com>
2025-05-16 18:07:59 -0400

    Adding a Micropub token endpoint

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html dir="ltr" lang="en-US">
 <head>
   <meta charset="UTF-8">
@@ -11,10 +12,11 @@
 
   <meta name="description" content="Personal site & portfolio of Jonathan Stephens: designer, writer, developer, photographer, and reader...with many piles of many books in many places.">
 
-  <!-- Webmentions -->
+  <!-- Webmentions & Indieweb-->
   <link rel="pingback" href="https://webmention.io/jonathanstephens.us/xmlrpc" />
   <link rel="webmention" href="https://webmention.io/jonathanstephens.us/webmention" />
   <link rel="micropub" href="https://jonathanstephens.us/micropub">
+  <link rel="token_endpoint" href="https://tokens.indieauth.com/token">
   <link rel="authorization_endpoint" href="https://indieauth.com/auth">
   <link href="https://github.com/jonathan-stephens" rel="me">
   <?php snippet('webmention-endpoint'); ?>

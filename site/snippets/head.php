cd3e17c78432bd2a6b082e6c6355a2032f6fe4ff
seasonllybraind <80056392+seasonllybraind@users.noreply.github.com>
2025-05-16 16:34:18 -0400

    whitespace fix

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -4,7 +4,6 @@
   <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-
   <title>
     <?= $site->title()->esc() ?> | <?php if($page->isHomePage()): ?>Home<?php else: ?><?= $page->title()->esc() ?><?php endif ?>
 

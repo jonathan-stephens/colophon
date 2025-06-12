27d1afaea158101351ff32457e7ed3a99963c2f2
seasonllybraind <80056392+seasonllybraind@users.noreply.github.com>
2025-06-12 09:51:17 -0400

    Separated the head into snippeted components to have more flexibility for future one-off pages of what gets included.

---
+++ site/snippets/head/styles.php
@@ -0,0 +1,12 @@
+<?= css([
+  'assets/css/main.css',
+]) ?>
+<?= css('/media/plugins/mauricerenck/komments/komments.css', ['defer' => true]) ?>
+
+<noscript>
+  <style>
+    #theme-picker {
+      display: none;
+    }
+  </style>
+</noscript>
---
+++ site/snippets/head/timekeeper.php
@@ -0,0 +1,4 @@
+<?php
+  $period = TimeKeeper::getCurrentPeriod();
+  $season = TimeKeeper::getCurrentSeason();
+?>

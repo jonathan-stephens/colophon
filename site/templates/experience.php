004d70b13e08a274c3abf421350a3ca13508e454
seasonllybraind <80056392+seasonllybraind@users.noreply.github.com>
2025-06-13 14:38:19 -0400

    Adding experience type block for creating work/experience

---
+++ site/templates/experience.php
@@ -0,0 +1,18 @@
+<?php snippet('header') ?>
+
+  <article class="h-resume post wrapper" itemscope itemtype="http://schema.org/Article">
+    <header>
+      <h1 class="p-name" itemprop="name headline"><?= $page->hed()->html() ?></h1>
+      <p><?= $page->dek()->html() ?><p>
+    </header>
+
+    <?php foreach ($page->blocks()->toBlocks() as $block): ?>
+
+    <div id="<?= $block->id() ?>" class="block block-type-<?= $block->type() ?>">
+      <?= $block ?>
+    </div>
+
+    <?php endforeach ?>
+  </article>
+
+<?php snippet('footer') ?>

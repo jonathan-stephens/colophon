18300b94a138805d1e27c1e2d515d8707cfa924a
seasonllybraind <80056392+seasonllybraind@users.noreply.github.com>
2025-07-20 11:43:13 -0400

    Adding favicons

--- site/snippets/head/meta.php
+++ site/snippets/head/meta.php
@@ -3,6 +3,18 @@
 <meta name="viewport" content="width=device-width, initial-scale=1" />
 <meta name="description" content="Personal site & portfolio of Jonathan Stephens: designer, writer, developer, photographer, and reader...with many piles of many books in many places.">
 
+
+<!-- Favicons -->
+<link rel="icon" type="image/x-icon" href="<?= $site->url() ?>/assets/favicon.ico">
+<link rel="icon" type="image/png" sizes="16x16" href="<?= $site->url() ?>/assets/favicon-16x16.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?= $site->url() ?>/assets/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="48x48" href="<?= $site->url() ?>/assets/favicon-48x48.png">
+<link rel="icon" type="image/svg+xml" href="<?= $site->url() ?>/assets/favicon.svg">
+<link rel="apple-touch-icon" sizes="180x180" href="<?= $site->url() ?>/assets/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="192x192" href="<?= $site->url() ?>/assets/android-chrome-192x192.png">
+<link rel="icon" type="image/png" sizes="512x512" href="<?= $site->url() ?>/assets/android-chrome-512x512.png">
+<meta name="theme-color" content="#FBF8EF">
+
 <!-- Facebook Meta Tags -->
 <meta property="og:url" content="https://jonathanstephens.us">
 <meta property="og:title" content="Jonathan Stephens">

460a8ca7096e610e51d39eb005d1e7da7474a258
seasonllybraind <80056392+seasonllybraind@users.noreply.github.com>
2025-07-29 10:30:08 -0400

    Bridgy suggested a different way of doing this, specifically in a class rather than a rel. doing that instead.

--- site/snippets/post-prose.php
+++ site/snippets/post-prose.php
@@ -1,6 +1,6 @@
 <div class="e-content prose" itemprop="articleBody">
   <?php if(!$page->website()->isEmpty()): ?>
-    <p class="written-url">URL: <a class="u-bookmark-of"  rel="in-reply-to" href="<?= $page->website()->html() ?>"><?= $page->website()->html() ?></a></p>
+    <p class="written-url">URL: <a class="u-bookmark-of u-in-reply-to"  href="<?= $page->website()->html() ?>"><?= $page->website()->html() ?></a></p>
   <?php endif?>
 
   <?= $page->text()->footnotes() ?>

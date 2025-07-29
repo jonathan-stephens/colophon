69ee5b20b71693a08f9a96eaa32a14399be98f03
seasonllybraind <80056392+seasonllybraind@users.noreply.github.com>
2025-07-29 10:28:53 -0400

    Added in-reply-to my /links/$page to better post and mark up for microformats and webmentions

--- site/snippets/post-prose.php
+++ site/snippets/post-prose.php
@@ -1,8 +1,8 @@
 <div class="e-content prose" itemprop="articleBody">
   <?php if(!$page->website()->isEmpty()): ?>
-    <p class="written-url">URL: <a class="u-bookmark-of" href="<?= $page->website()->html() ?>"><?= $page->website()->html() ?></a></p>
+    <p class="written-url">URL: <a class="u-bookmark-of"  rel="in-reply-to" href="<?= $page->website()->html() ?>"><?= $page->website()->html() ?></a></p>
   <?php endif?>
 
   <?= $page->text()->footnotes() ?>
-  
+
 </div>

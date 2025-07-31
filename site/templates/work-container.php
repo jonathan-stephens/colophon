1450792f0da1136a6b29f67104f98e4593f2b185
seasonllybraind <80056392+seasonllybraind@users.noreply.github.com>
2025-07-31 17:46:34 -0400

    Update work-container.php

--- site/templates/work-container.php
+++ site/templates/work-container.php
@@ -2,16 +2,17 @@
 
 <article class="article post h-entry wrapper" itemscope itemtype="http://schema.org/Article">
   <header class="work">
-    <h1>Selected Case Studies</h1>
+    <h1>Work</h1>
 
-    <p class="dek">Guiding cross-functional product development through inclusive design leadership and systematic execution.</p>
+    <p class="dek">Guiding cross-functional product development through inclusive design leadership and systematic iteration.</p>
     <div class="lede">
-      <p>My approach combines strategic thinking with hands-on execution, delivering everything from design systems to complete product transformations.</p>
+      <p>My approach combines strategic thinking with hands-on execution, delivering everything from design systems to holistic product life-cycle.</p>
       <p>I focus on connecting situational strengths to organizational goals, creating accessible experiences that drive measurable business growth.</p>
     </div>
   </header>
 
+  <h2>Selected Case Studies</h2>
   <?php snippet('/components/case-studies', ['hedLevel' => 3]) ?>
 
-  <?php snippet('post-footer') ?>
+
 <?php snippet('site-footer') ?>

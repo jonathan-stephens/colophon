6f7115842ca8ea299767467a68828ebe8f6a753a
seasonllybraind <80056392+seasonllybraind@users.noreply.github.com>
2025-08-12 10:27:36 -0400

    Updated /work-container to have some professional statistics, some padding, and renamed the header to Selected Work to move away from explicit "case studies"

--- site/templates/work-container.php
+++ site/templates/work-container.php
@@ -4,13 +4,42 @@
   <header class="masthead work">
     <h1><?= $page->title()->html() ?></h1>
 
-    <p class="dek"><?= $page->dek()->html() ?></p>
+    <p class="dek"><span><?= $page->dek()->html() ?></span></p>
+
     <div class="lede">
       <?= $page->lede()->kt() ?>
     </div>
+    <ul class="metrics">
+      <li>
+        <span>18+</span>
+        Years of experience
+      </li>
+      <li>
+        <span>10+</span>
+        Years in leadership
+      </li>
+      <li>
+        <span>~100</span>
+        Largest org managed
+      </li>
+      <li>
+        <span>~150</span>
+        Performance reviews
+      </li>
+      <li>
+        <span>40+</span>
+        Promotions awarded
+      </li>
+      <li>
+        <span>~40</span>
+        Teams managed
+      </li>
+    </ul>
+
   </header>
 
-  <h2>Selected Case Studies</h2>
+
+  <h2>Selected Work</h2>
   <?php snippet('/components/case-studies', ['hedLevel' => 3]) ?>
 
 

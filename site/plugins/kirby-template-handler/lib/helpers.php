42a5ef13400557683e8dd248fbf8671d5e1f5ef9
Jonathan Stephens <jonathan@poetandscribe.com>
2025-09-21 14:06:06 -0400

    removing this from everything, have moved past it and better understand kirby now

--- site/plugins/kirby-template-handler/lib/helpers.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * Helper function to use in templates
- */
-function useTemplate($conditions = []) {
-    $handler = new TemplateHandler();
-    return $handler->getTemplate($conditions);
-}

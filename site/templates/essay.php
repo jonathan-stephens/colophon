6b7b80638e9ac6dfa2511a618b9f68492ea0e212
seasonllybraind <80056392+seasonllybraind@users.noreply.github.com>
2025-12-11 11:09:45 -0500

    testing the comments to get them ilive

--- site/templates/essay.php
+++ site/templates/essay.php
@@ -1,5 +1,12 @@
 <?php snippet('site-header') ?>
   <?php snippet('post-header') ?>
   <?php snippet('post-prose') ?>
+  <!-- Putting comments here to test putting them into the site -->
+  <?php snippet('komments/kommentform'); ?>
+  <?php snippet('komments/list/likes'); ?>
+  <?php snippet('komments/list/reposts'); ?>
+  <?php snippet('komments/list/replies'); ?>
+  <?php snippet('komments/list/comments'); ?>
+  
   <?php snippet('post-footer') ?>
 <?php snippet('site-footer') ?>

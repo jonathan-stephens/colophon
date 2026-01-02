3750bda1d8695b5e1e022c343b1dcba828fb616f
seasonllybraind <80056392+seasonllybraind@users.noreply.github.com>
2026-01-02 14:10:21 -0500

    Update copyright.php

--- site/snippets/copyright.php
+++ site/snippets/copyright.php
@@ -1,7 +1,7 @@
 <p class="h-card">
   <span class="with-icon">
     <?php echo asset('assets/svg/icons/copyright.svg')->read() ?>
-  2008&thinsp;&ndash;&thinsp;2025</span>
+  2008&thinsp;&ndash;&thinsp;2026</span>
     <a class="p-name u-url" href="https://jonathanstephens.us">Jonathan Stephens</a> |
     <a class="u-email" href="mailto://hello@jonathanstephens.us">
       <span class="with-icon">

373a8ad226e5b09cfa709f114dd5a35a96ab73da
seasonllybraind <80056392+seasonllybraind@users.noreply.github.com>
2026-03-10 11:42:31 -0400

    Update styles.php

--- site/snippets/head/styles.php
+++ site/snippets/head/styles.php
@@ -1,8 +1,8 @@
   <!-- Styles -->
-    <link rel="preload" href="<?= Bnomei\Fingerprint::url('/assets/fonts/ibm-plex-sans/variable-roman.woff2');?>" as="font" type="font/woff2" crossorigin>
-    <link rel="preload" href="<?= Bnomei\Fingerprint::url('/assets/fonts/ibm-plex-sans/variable-italic.woff2');?>" as="font" type="font/woff2" crossorigin>
-    <link rel="preload" href="<?= Bnomei\Fingerprint::url('/assets/fonts/ibm-plex-serif/serif-medium.woff2'); ?>" as="font" type="font/woff2" crossorigin>
-    <link rel="preload" href="<?= Bnomei\Fingerprint::url('/assets/fonts/ibm-plex-serif/serif-bold.woff2'); ?>" as="font" type="font/woff2" crossorigin>
+    <link href="<?= Bnomei\Fingerprint::url('/assets/fonts/ibm-plex-sans/variable-roman.woff2');?>" as="font" type="font/woff2" crossorigin>
+    <link href="<?= Bnomei\Fingerprint::url('/assets/fonts/ibm-plex-sans/variable-italic.woff2');?>" as="font" type="font/woff2" crossorigin>
+    <link href="<?= Bnomei\Fingerprint::url('/assets/fonts/ibm-plex-serif/serif-medium.woff2'); ?>" as="font" type="font/woff2" crossorigin>
+    <link href="<?= Bnomei\Fingerprint::url('/assets/fonts/ibm-plex-serif/serif-bold.woff2'); ?>" as="font" type="font/woff2" crossorigin>
 
     <?= Bnomei\Fingerprint::css('/assets/css/main.css'); ?>
     <?= Bnomei\Fingerprint::css('/media/plugins/mauricerenck/komments/komments.css', ['defer' => true]) ?>

6978bea0a9a610a93e8f7f9e2132506af2d7c022
seasonllybraind <80056392+seasonllybraind@users.noreply.github.com>
2026-03-10 12:26:51 -0400

    Update styles.php

--- site/snippets/head/styles.php
+++ site/snippets/head/styles.php
@@ -4,5 +4,5 @@
     <link href="<?= Bnomei\Fingerprint::url('/assets/fonts/ibm-plex-serif/serif-medium.woff2'); ?>" as="font" type="font/woff2" crossorigin>
     <link href="<?= Bnomei\Fingerprint::url('/assets/fonts/ibm-plex-serif/serif-bold.woff2'); ?>" as="font" type="font/woff2" crossorigin>
 
-    <?= Bnomei\Fingerprint::css('/assets/css/main.css', ['preload' => true]); ?>
+    <?= Bnomei\Fingerprint::css('/assets/css/main.css'); ?>
     <?= Bnomei\Fingerprint::css('/media/plugins/mauricerenck/komments/komments.css', ['defer' => true]) ?>
